5cf7e5e677a4c4f2c1bf87725d19b429a7af3489
andrews1399 <86428986+andrews1399@users.noreply.github.com>
2022-05-12 11:44:05 -0400

    Add files via upload

---
+++ postgres/gnuhealth.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+: ${GNUHEALTH_USER:=gnuhealth}
+if [ "$GNUHEALTH_PASSWORD" ]; then
+    PASS="PASSWORD '$GNUHEALTH_PASSWORD'"
+else
+    PASS="PASSWORD '$GNUHEALTH_USER'"
+fi
+: ${GNUHEALTH_DB:=health30}
+: ${DB_ENCODING:=UTF-8}
+: ${DB_DUMPFILE:=/demo.sql.gz}
+
+# Perform all actions as user 'postgres'
+export PGUSER=postgres
+
+# Test if the gnuhealth database already exists
+echo "Checking for existing database..."
+INIT=$(psql -d template1 -t <<-EOSQL
+        SELECT COUNT(*) from pg_database where datname = '$GNUHEALTH_DB';
+EOSQL
+)
+INIT="$(echo "$INIT" | sed -e 's/^[ \t]*//;s/[ \t]*$//')"
+
+if [ "${INIT}" == "0" ]; then
+	echo "Creating the gnuhealth role..."
+	psql <<-EOSQL
+		CREATE USER $GNUHEALTH_USER WITH NOSUPERUSER NOINHERIT CREATEDB NOCREATEROLE NOREPLICATION $PASS;
+	EOSQL
+	echo
+	echo "Creating the gnuhealth database..."
+	psql <<-EOSQL
+		CREATE DATABASE $GNUHEALTH_DB WITH OWNER $GNUHEALTH_USER ENCODING='$DB_ENCODING';
+	EOSQL
+	echo
+	echo "Importing the gnuhealth database..."
+	{ gosu postgres gunzip -c "$DB_DUMPFILE" | psql -d "$GNUHEALTH_DB"; }
+	echo
+	echo "Allowing access for the gnuhealth user from all IPs..."
+	{ echo; echo "host all $GNUHEALTH_USER 0.0.0.0/0 trust"; } >> "$PGDATA"/pg_hba.conf
+	echo
+	echo "GNUHealth database ready for startup."
+fi
